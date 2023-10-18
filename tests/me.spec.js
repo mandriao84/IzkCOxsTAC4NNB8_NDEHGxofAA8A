b2058dc29a9366bede5a9bcf3eb1e9645c8ca337
GREGORY LOURENCO <glouren@hotmail.com>
2023-10-18 14:30:43 +0200

    g

--- tests/me.spec.js
+++ tests/me.spec.js
@@ -1,7 +1,6 @@
 const { test, expect } = require('@playwright/test')
-const fs = require('fs')
 const hlp = require(process.cwd() + '/helpers/general/helpers.js')
 
-test('after ....', async ({ context, page, request }) => {
+test('after ...', async ({ context, page, request }) => {
   await hlp._results(request)
 })
