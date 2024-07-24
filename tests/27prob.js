4f85cf5645545de4c09e41b797ecdcf24f6d26a7
GREGORY LOURENCO <glouren@hotmail.com>
2024-07-24 15:55:40 +0100

    g

--- tests/27prob.js
+++ tests/27prob.js
@@ -1,4 +1,4 @@
-const getDrawsProbability = (numberOfRoundsLeft, numberOfCardsDiscarded, numberOfCardsLeft) => {
+const getDrawsProbability = (numberOfRoundsLeft, numberOfCardsDiscarded, numberOfCardsDesired) => {
     const cardsTotal = 52;
     const handSize = 5;
 
@@ -7,16 +7,16 @@ const getDrawsProbability = (numberOfRoundsLeft, numberOfCardsDiscarded, numberO
         const cardsLeft = cardsTotal - handSize;
 
         // // probability of drawing 1 card | ex: 16/47
-        // const p1 = numberOfCardsLeft / cardsLeft;
+        // const p1 = numberOfCardsDesired / cardsLeft;
         // // probability of drawing 2 cards | ex: 15/46
-        // const p2 = (numberOfCardsLeft - 1) / (cardsLeft - 1);
+        // const p2 = (numberOfCardsDesired - 1) / (cardsLeft - 1);
         // // probability of drawing 3 cards | ex: 14/45
-        // const p3 = (numberOfCardsLeft - 2) / (cardsLeft - 2);
+        // const p3 = (numberOfCardsDesired - 2) / (cardsLeft - 2);
         // return p1 * p2 * p3;
 
         let result = 1;
         for (let i = 0; i < numberOfCardsDiscarded; i++) { 
-            const p = (numberOfCardsLeft - i) / (cardsLeft - i);
+            const p = (numberOfCardsDesired - i) / (cardsLeft - i);
             result *= p;
         }
         return result
@@ -32,9 +32,9 @@ const getDrawsProbability = (numberOfRoundsLeft, numberOfCardsDiscarded, numberO
 
 
 const numberOfRoundsLeft = 3;
-const numberOfCardsDiscarded = 0;
-const numberOfCardsLeft = 16; // 4 each of 3, 4, 5, 6
+const numberOfCardsDiscarded = 3;
+const numberOfCardsDesired = 16; // 4 each of 3, 4, 5, 6
 for (let i = 1; i <= numberOfRoundsLeft; i++) {
-    const probabilityOfSuccess = getDrawsProbability(i, numberOfCardsDiscarded, numberOfCardsLeft);
+    const probabilityOfSuccess = getDrawsProbability(i, numberOfCardsDiscarded, numberOfCardsDesired);
     console.log(`- Probability to draw in ${i} round${i > 1 ? 's' : ''}: ${(probabilityOfSuccess * 100).toFixed(2)}%`);
 }
