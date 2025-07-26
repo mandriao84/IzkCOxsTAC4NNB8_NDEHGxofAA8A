908d884229c8bd9ae786db4c8a4733031b3cc150
GREGORY LOURENCO <glouren@hotmail.com>
2025-07-26 10:51:08 +0200

    g

--- tests/MCCFR27Discards2.js
+++ tests/MCCFR27Discards2.js
@@ -964,12 +964,12 @@ const getMCCFRComputed = async (roundNumber, roundNumbersFrozen) => {
     // console.log(HANDS_CANONICAL_INDEX.length);
 
 
-    const roundNumber = 1;
-    /** (roundNumbersFrozen) >>
-     * PUT 1 ON ARRAY INDEX THAT MATCH ROUND TO FREEZE
-     * INDEX 0 === 0 */ 
-    const roundNumbersFrozen = new Uint8Array([0, 0, 0, 0]); 
-    getMCCFRComputed(roundNumber, roundNumbersFrozen);
+    // const roundNumber = 1;
+    // /** (roundNumbersFrozen) >>
+    //  * PUT 1 ON ARRAY INDEX THAT MATCH ROUND TO FREEZE
+    //  * INDEX 0 === 0 */ 
+    // const roundNumbersFrozen = new Uint8Array([0, 0, 0, 0]); 
+    // getMCCFRComputed(roundNumber, roundNumbersFrozen);
 
 
     // [
@@ -980,11 +980,11 @@ const getMCCFRComputed = async (roundNumber, roundNumbersFrozen) => {
     //     getDataFlushedMerged(dir)
     // })
 
-    // getDataNashed();
-    // [MCCFR] NASH_BELOW_0.02=13606 / 14469
+    getDataNashed();
+    // [MCCFR] NASH_BELOW_0.02=13671 / 14469
     // [MCCFR] NASH_BELOW_0.05=14469 / 14469
-    // [MCCFR] NASH_AVERAGE=0.009705764790312226
-    // [MCCFR] NASH_MAX=0.035200519377606494
+    // [MCCFR] NASH_AVERAGE=0.00951273516917342
+    // [MCCFR] NASH_MAX=0.03484117835418409
 })();
 
 // const hand = ["6s", "4h", "6d", "4s", "7c"]
