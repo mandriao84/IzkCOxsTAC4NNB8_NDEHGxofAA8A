80829f8b44757c42d812b93f363d6f0e98cc351d
GREGORY LOURENCO <glouren@hotmail.com>
2025-08-01 09:57:14 +0200

    g

--- tests/MCCFR27Discards2.js
+++ tests/MCCFR27Discards2.js
@@ -981,10 +981,10 @@ const getMCCFRComputed = async (roundNumber, roundNumbersFrozen) => {
     // })
 
     // getDataNashed();
-    // [MCCFR] NASH_BELOW_0.02=13942 / 14469
+    // [MCCFR] NASH_BELOW_0.02=14005 / 14469
     // [MCCFR] NASH_BELOW_0.05=14469 / 14469
-    // [MCCFR] NASH_AVERAGE=0.008809252003031546
-    // [MCCFR] NASH_MAX=0.0329344139459993
+    // [MCCFR] NASH_AVERAGE=0.008596971310030569
+    // [MCCFR] NASH_MAX=0.030055286525020732
 })();
 
 // const hand = ["6s", "4h", "6d", "4s", "7c"]
