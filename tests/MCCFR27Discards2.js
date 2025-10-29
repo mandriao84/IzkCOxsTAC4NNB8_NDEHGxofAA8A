a7b2dec8182f4c8ca3a3cce3a3ee913854317932
GREGORY LOURENCO <glouren@hotmail.com>
2025-10-30 00:48:52 +0100

    g

--- tests/MCCFR27Discards2.js
+++ tests/MCCFR27Discards2.js
@@ -458,7 +458,7 @@ const getCacheCreated = (roundNumber) => {
     getNDJSONAsMap(".results/mccfr/evs/__REF.ndjson", evSum, Int32Array);
     getNDJSONAsMap(".results/mccfr/regrets/__REF.ndjson", regretSum, Float32Array);
     getNDJSONAsMap(".results/mccfr/strategies/__REF.ndjson", strategySum, Float32Array);
-    const evSumBottomLow = (evSum.size * 0.1).safe("ROUND", 0);
+    const evSumBottomLow = (evSum.size * 0.02).safe("ROUND", 0);
     const evSumEntries = Array.from(evSum.entries());
     evSumEntries.sort((a, b) => a[1][0] - b[1][0]);
 
