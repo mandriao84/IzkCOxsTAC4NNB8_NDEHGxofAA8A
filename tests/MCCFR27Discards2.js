4e926bb564d453459149a716bfd43daa6100e615
GREGORY LOURENCO <glouren@hotmail.com>
2025-10-30 00:53:40 +0100

    g

--- tests/MCCFR27Discards2.js
+++ tests/MCCFR27Discards2.js
@@ -915,7 +915,7 @@ const getMCCFRComputed = async (roundNumber, roundNumbersFrozen) => {
         getCacheCreated(roundNumber);
 
         const flushInterval = HANDS_CANONICAL_INDEX.length;
-        const iterations = 1_00;
+        const iterations = 10_000;
         let timeNow = performance.now();
 
         const deckRef = Object.values(DECK);
